dd4142240c45afd3aad057fe716c5165757685fc
louwenus <louwen.fricout@gmail.com>
2023-07-11 13:01:12 +0200

    acessing values ...

--- lib/recursive_sim_array_c.c
+++ lib/recursive_sim_array_c.c
@@ -1,1 +1,56 @@
 #include "recursive_sim_array_c.h"
+#include <unistd.h>
+
+
+
+cell* access(recursive_array* array,short level,int x,int y)
+//access a cell considering x/y 0 are upper left of array. try to acess parent if needed
+//return NULL if unedefined (hitting a NULL while searching)
+{
+    if (array == NULL){
+        return NULL;
+    }
+    int size=1<<level;
+    while (size!=1){
+        if ((x<0) || (y<0) || (y>=size) || (x>=size)){ //x or y are out of bound, going upper in the recursive structure
+            recursive_array* upper=(*array).parent;
+            if (upper == NULL){
+                return NULL;
+            }
+            if (array==(*upper).upright.sub_array){
+                x-=size;
+            } else if (array==(*upper).downleft.sub_array){
+                y-=size;
+            } else if (array==(*upper).downright.sub_array){
+                x-=size;
+                y-=size;
+            }
+
+            array=upper;
+            size=size<<1;
+        } else {     //x and y are in this structure, finding in wich quarter
+            size=size>>1;
+            if (x>=size){
+                x-=size;
+                if (y>=size){
+                    y-=size;
+                    array=(*array).downright.sub_array;
+                } else {
+                    array=(*array).upright.sub_array;
+                }
+            } else {
+                if (y>=size){
+                    y-=size;
+                    array=(*array).downleft.sub_array;
+                } else {
+                    array=(*array).upleft.sub_array;
+                }
+            }
+            if (array==NULL){
+                return NULL;
+            }
+        }
+    }
+    //in fact, we are at lowest level, so this is a cell*, not a subarray
+    return (cell*)array;
+};
--- lib/recursive_sim_array_c.h
+++ lib/recursive_sim_array_c.h
@@ -1,11 +1,20 @@
-typedef union {struct recursive_array* sub_array; short int* data;} array_data;
+typedef struct
+{
+    short int cur;
+    short int next;
+} cell;
+
+
+typedef union {struct recursive_array* sub_array; cell* data;} array_data;
 
 struct recursive_array
 {
-    array_data* upperright;  //most sub level array contain int*, 
-    array_data* upperleft;   //others contains recursive_array* (or nullptr for uninitialized parts)
-    array_data* downleft;    //which mean, along with the array must be passed
-    array_data* downright;   //how much level of recusion he contain
+    array_data upright;     //most sub level array contain cell*, (level 1)
+    array_data upleft;      //others contains recursive_array* (or nullptr for uninitialized parts)
+    array_data downleft;    //which mean, along with the array must be passed
+    array_data downright;   //how much level of recusion he contain
 
     recursive_array* parent; //used for fast traversal, nullptr for the most outer one
 };
+
+typedef recursive_array recursive_array;
