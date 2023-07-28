06c6b7273823d788792f2d282272b23c4bea8072
louwenus <louwen.fricout@gmail.com>
2023-07-28 20:44:03 +0200

    working ...

--- lib/cy_sim.pyx
+++ lib/cy_sim.pyx
@@ -1,21 +1,30 @@
 # cython: infer_types=True
 import numpy as np
 cimport numpy as cnp
+cimport cython
+
 
 cnp.import_array()
 DTYPE = np.int16
 ctypedef cnp.int16_t DTYPE_t
 
-cdef int simulate_one(const DTYPE_t[:,:] cells):
-    #cells should be a 3*3 memoryview, return new value for center
-    return cells[1,1] #TODO!!! return same value for now
+
+@cython.boundscheck(False) # turn off bounds-checking for entire function
+@cython.wraparound(False)  # turn off negative index wrapping for entire function
+cdef bint simulate_one(DTYPE_t[:,:] cells) nogil:
+    #cells should be a 3*3 memoryview, may change center value, return True if, (and only if) center value changed
+    return False
+
 
 class chunk():
     def __init__(self,size:int,up:chunk=None,down:chunk=None,right:chunk=None,left:chunk=None) -> None:
-        outersize=size+2
-        self.nparray=np.zeros((outersize,outersize,2),dtype=np.short)
+
+        outersize:int=size+2
+        cdef cnp.ndarray nparray=np.zeros((outersize,outersize,2),dtype=np.short)
+        self.nparray=nparray
         cdef int[:,:,:] memview = self.nparray
         self.memview=memview
+
         self.up=up
         self.down=down
         self.right=right
@@ -31,6 +40,9 @@ class chunk():
             self.right=right
         if left!=None:
             self.left=left
+    
+    @cython.boundscheck(False) # turn off bounds-checking for entire function
+    @cython.wraparound(False)  # turn off negative index wrapping for entire function
     cdef simulate(self,isodd:bint) nogil:
         #on ODD: [:,:,1]->[:,:,0], reverse on non ODD
         cdef short source,dest
@@ -45,7 +57,7 @@ class chunk():
         looprange=self.size+1
         for x in range(1,looprange):
             for y in range(1,looprange):
-                pass
+                simulate_one(self.memview[x-1:x+1,y-1:y+1,source])
 
     def py_direct_simulate(self,isodd:bool):
         self.simulate(isodd)
